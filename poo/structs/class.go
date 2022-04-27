e10015cc8171d5e21ad1b1c510bebaff7abdb7e7
DiegoAll <diego58229@hotmail.com>
2022-04-27 00:42:15 -0500

    Metodos y Funciones

--- poo/structs/class.go
+++ poo/structs/class.go
@@ -7,10 +7,34 @@ type Employee struct {
 	name string
 }
 
+func (e *Employee) SetId(id int) {
+	e.id = id
+}
+
+func (e *Employee) SetName(name string) {
+	e.name = name
+}
+
+//Recordar que en Go hay que especificar el tipo de retorno.
+func (e *Employee) GetId() int {
+	return e.id
+}
+
+func (e *Employee) GetName() string {
+	return e.name
+}
+
 func main() {
 	e := Employee{}
-	fmt.Printf("%+v\n", e)
+	//fmt.Printf("%+v\n", e)
 	e.id = 1
 	e.name = "Pedro"
+	//e.SetId(5)
 	fmt.Printf("%+v\n", e)
+	e.SetId(7)
+	//fmt.Printf("%+v\n", e)
+	e.SetName("Jonas")
+	//fmt.Printf("%+v\n", e)
+	fmt.Println(e.GetId())
+	fmt.Println(e.GetName())
 }
