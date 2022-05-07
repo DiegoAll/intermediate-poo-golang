a9d280dcf0c259ac2dc3fa5e557156640067f29f
DiegoAll <diego58229@hotmail.com>
2022-05-07 17:04:58 -0500

    Interfaces

--- poo/inheritance/class.go
+++ poo/inheritance/class.go
@@ -14,6 +14,31 @@ type Employee struct {
 type FullTimeEmployee struct {
 	Person   //Campo anónimo
 	Employee //Campo anónimo
+	endDate  string
+}
+
+func (ftEmployee FullTimeEmployee) GetMessage() string {
+	return "Full Time Employee"
+}
+
+type TemporaryEmployee struct {
+	Person
+	Employee
+	taxRate int
+}
+
+func (tEmployee TemporaryEmployee) GetMessage() string {
+	return "Temporary Employee"
+}
+
+type PrintInfo interface {
+	GetMessage() string
+}
+
+// Receives interface as parameter Target: Polymorphism
+// Here the interface is being implemented
+func getMessage(p PrintInfo) {
+	fmt.Println(p.GetMessage())
 }
 
 func GetMessage(p Person) {
@@ -26,5 +51,10 @@ func main() {
 	ftEmployee.age = 10
 	ftEmployee.id = 55555555
 	fmt.Printf("%v\n", ftEmployee)
-	GetMessage(ftEmployee)
+	// Error because in Golang it's not possible apply polymorphism without interfaces.
+	// GetMessage(ftEmployee)
+
+	tEmployee := TemporaryEmployee{}
+	getMessage(tEmployee)
+	getMessage(ftEmployee)
 }
